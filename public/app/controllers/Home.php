656a08652d1147db12493643c7709fabf50f51a2
Le Cong Danh <lcdanh996@gmail.com>
2023-04-17 08:20:06 +0900

    connect to DB using singleton partern

--- public/app/controllers/Home.php
+++ public/app/controllers/Home.php
@@ -5,7 +5,7 @@ class Home extends Controller{
         parent::__construct($controller, $action);
     }
 
-    public function indexAction($name){
+    public function indexAction(){
         $this->view->render('home/index');
     }
 }
--- public/config/config.php
+++ public/config/config.php
@@ -1,5 +1,11 @@
 <?php
 define('DEBUG', true);
+
+define('DB_NAME', 'laravel_test');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'password');
+define('DB_HOST', '127.0.0.1');
+
 define('DEFAULT_CONTROLLER', 'Home');
 define('DEFAULT_LAYOUT', 'default');
 define('PROOT', '/');
