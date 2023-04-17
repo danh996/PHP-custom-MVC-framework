1d4a5da06ee3b39324b5b1047164b2f5e42d3b03
Le Cong Danh <lcdanh996@gmail.com>
2023-04-17 14:04:38 +0900

    CRUD implement for models

--- public/core/Model.php
+++ public/core/Model.php
@@ -49,6 +49,64 @@ class Model{
         ]);
     }
 
+    public function save(){
+        $fields = [];
+        foreach ($this->_columnNames as $column){
+            $fields[$column] = $this->$column;
+        }
+
+//        determine whether to update or insert
+        if(property_exists($this, 'id') && $this->id!=''){
+            return $this->update($this->id, $fields);
+        } else {
+            $this->insert($fields);
+        }
+    }
+
+    public function insert($fields){
+        if(empty($fields)) return false;
+        return $this->_db->insert($this->_table, $fields);
+    }
+
+    public function update($id, $fields){
+        if(empty($fields) || $id =='') return false;
+        return $this->_db->update($this->_table, $id, $fields);
+    }
+
+    public function delete($id, $fields){
+        if($id == '' && $this->id == '') return false;
+        if($id == '') $id == $this->id;
+        if($this->_softDelete){
+            return $this->update($id, ['delete'=>1]);
+        }
+        return $this->_db->delete($this->_table, $id);
+    }
+
+    public function query($sql, $bind){
+        return $this->_db->query($sql, $bind);
+    }
+
+    public function data()
+    {
+        $data = new stdClass();
+        foreach ($this->_columnNames as $column){
+            $data->column = $this->column;
+        }
+        return $data;
+    }
+
+    public function assign($params){
+        if(!empty($params)){
+            foreach ($params as $key => $val){
+                if(in_array($key, $this->_columnNames)){
+                    $this->$key = sanitize($val);
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
     protected function populatedObjData($result){
         foreach ($result as $key => $val){
             $this->$key = $val;
